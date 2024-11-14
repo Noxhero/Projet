f80d952e989e438dd3d2a12fd5682ea37a6b12a0
RaphaelJ19 <135717497+RaphaelJ19@users.noreply.github.com>
2024-11-14 14:59:33 +0100

    Update traitement.cavalerie.php

--- pages/cavalerie/traitement.cavalerie.php
+++ pages/cavalerie/traitement.cavalerie.php
@@ -1,9 +1,23 @@
 <?php
-
 include '../../includes/haut.inc.php';
+include_once '../../pages/cavalerie/cavalerie.class.php';
 
+// Modification d'un cheval
+if (isset($_POST['action']) && $_POST['action'] === 'modifier') {
+    $numsire = $_POST["numsire"];
+    $nomcheval = $_POST["nomcheval"];
+    $datenaissancecheval = $_POST["datenaissancecheval"];
+    $garot = $_POST["garot"];
+    $idrobe = $_POST["idrobe"];
+    $idrace = $_POST["idrace"];
 
-// Ajout d'un nouveau cavalier
+    $unCheval = new Cavalerie($numsire, $nomcheval, $datenaissancecheval, $garot, $idrobe, $idrace);
+    $unCheval->updateCheval();
+    header("Location: cavalerie.php");
+    exit();
+}
+
+// Ajout d'un nouveau cheval
 if (isset($_POST["nomcheval"]) && isset($_POST["datenaissancecheval"]) && isset($_POST["garot"])) {
     $nomcheval = $_POST["nomcheval"];
     $datenaissancecheval = $_POST["datenaissancecheval"];
@@ -11,21 +25,23 @@ if (isset($_POST["nomcheval"]) && isset($_POST["datenaissancecheval"]) && isset(
     $idrobe = $_POST["idrobe"];
     $idrace = $_POST["idrace"];
 
-        
-
-    $unCavalier = new Cavalerie(null, $nomcheval, $datenaissancecheval, $garot, $idrobe, $idrace);
-    $unCavalier->insertCheval();
-    
+    $unCheval = new Cavalerie(null, $nomcheval, $datenaissancecheval, $garot, $idrobe, $idrace);
+    $unCheval->insertCheval();
     header("Location: cavalerie.php");
-    exit(); 
+    exit();
 }
-//Suppression d'un cavalier
+
+// Suppression d'un cheval
+// Suppression d'un cavalier
 if (isset($_POST["supprimer"])) {
     $idcavalerie = $_POST["supprimer"]; 
     $unCavalier = new Cavalerie($idcavalerie, null, null, null, null, null); 
-    $unCavalier->DeleteCavalerie($idCavalier);
+    $unCavalier->DeleteCavalerie($idcavalerie);  // Utiliser DeleteCavalerie ici
     
     header("Location: cavalerie.php");
     exit(); 
 }
 ?>
+
+
+
