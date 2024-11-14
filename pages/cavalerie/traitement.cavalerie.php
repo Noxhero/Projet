4536504def2773d76387e645f667e8b1e56c04f7
Noxhero <matteo.piera19@gmail.com>
2024-11-14 10:52:50 +0100

    Update traitement.cavalerie.php

--- pages/cavalerie/traitement.cavalerie.php
+++ pages/cavalerie/traitement.cavalerie.php
@@ -1,29 +1,31 @@
 <?php
 
 include '../../includes/haut.inc.php';
-require_once 'cavalerie.class.php';
 
-if (isset($_POST['action'])) {
-    $action = $_POST['action'];
 
-    $numsire = $_POST['numsire'];
-    $nomcheval = $_POST['nomcheval'];
-    $datenaissancecheval = $_POST['datenaissancecheval'];
-    $garot = $_POST['garot'];
-    $idrobe = $_POST['idrobe'];
-    $idrace = $_POST['idrace'];
+// Ajout d'un nouveau cavalier
+if (isset($_POST["nomcheval"]) && isset($_POST["datenaissancecheval"]) && isset($_POST["garot"])) {
+    $nomcheval = $_POST["nomcheval"];
+    $datenaissancecheval = $_POST["datenaissancecheval"];
+    $garot = $_POST["garot"];
+    $idrobe = $_POST["idrobe"];
+    $idrace = $_POST["idrace"];
 
-    $cheval = new Cavalerie($numsire, $nomcheval, $datenaissancecheval, $garot, $idrobe, $idrace);
+        
 
-    if ($action === 'ajouter') {
-        $cheval->insertCheval();
-    } elseif ($action === 'modifier') {
-        $cheval->updateCheval();
-    } elseif ($action === 'supprimer') {
-        $cheval->deleteCheval($numsire);
-    }
-
-    header('Location: cavalerie.php');
-    exit();
+    $unCavalier = new Cavalerie(null, $nomcheval, $datenaissancecheval, $garot, $idrobe, $idrace);
+    $unCavalier->insertCheval();
+    
+    header("Location: cavalerie.php");
+    exit(); 
+}
+//Suppression d'un cavalier
+if (isset($_POST["supprimer"])) {
+    $idcavalerie = $_POST["supprimer"]; 
+    $unCavalier = new Cavalerie($idcavalerie, null, null, null, null, null); 
+    $unCavalier->DeleteCavalerie($idCavalier);
+    
+    header("Location: cavalerie.php");
+    exit(); 
 }
 ?>
