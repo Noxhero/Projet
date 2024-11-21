ebbc2c99c59881ac690de1ddc7aa07b585d3ea50
Noxhero <matteo.piera19@gmail.com>
2024-11-21 11:55:40 +0100

    Update haut.inc.php

--- includes/haut.inc.php
+++ includes/haut.inc.php
@@ -4,98 +4,91 @@ session_start();
 
 // Vérification de la session utilisateur
 if (isset($_SESSION['user_id'])) {
-
     $session_idcompte = $_SESSION['user_id'];
-    
+
     // Requête pour vérifier si l'utilisateur existe toujours
     $query = $con->prepare("SELECT COUNT(*) FROM compte WHERE idcompte = :idcompte");
     $query->bindParam(':idcompte', $session_idcompte);
     $query->execute();
-    
+
     // Si l'utilisateur est valide, affichage du pseudo et du bouton de déconnexion
     if ($query->fetchColumn() > 0) {
-        echo "Utilisateur : " . $_SESSION['user_pseudo'];
-        echo"<br>";
-        // Bouton de déconnexion
-        echo '<form action="" method="post" style="all:unset">
-                <button type="submit" name="logout">Déconnexion</button>
-              </form>';
-
-        
-        
-        // Si le bouton est cliqué, détruire la session
-        if (isset($_POST['logout'])) {
-            session_destroy(); // Détruire la session
-            header('Location: ../connexion/connexion.php'); // Redirection vers la page de connexion
-            exit();
-        }
-
+        $user_pseudo = $_SESSION['user_pseudo'];
     } else {
         header('Location: ../connexion/connexion.php');
         exit();
     }
-
 } else {
     header('Location: ../connexion/connexion.php');
     exit();
 }
-?>
 
+// Si le bouton de déconnexion est cliqué, détruire la session
+if (isset($_POST['logout'])) {
+    session_destroy(); // Détruire la session
+    header('Location: ../connexion/connexion.php'); // Redirection vers la page de connexion
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
     <link rel="stylesheet" href="../../css/style.css">
     <script src="../../js/jquery.min.js"></script>
     <script src="../../js/script.js"></script>
-    <!--Inclusion du Tableaudata -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
-  
-
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-  <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-
-    <br><br>
-</head><header>
-    <div class="dropdown">
-  &nbsp;  <img src='../../assets/images/menu.png' width="30 %"class="dropbtn"></img>
-        <div class="dropdown-content">
-            <a href="../../pages/calendrier/calendrier.php">calendrier</a>
-            <a href="../../pages/cavalerie/cavalerie.php">cavalerie</a>
-            <a href="../../pages/cavalier/cavalier.php">cavalier</a>
-            <a href="../../pages/commune/commune.php">commune</a>
-            <a href="../../pages/cours/cours.php">cours</a>
-            <a href="../../pages/evenement/evenement.php">evenement</a>
-            <a href="../../pages/galop/galop.php">galop</a>
-            <a href="../../pages/participation/participation.php">participation</a>
-            <a href="../../pages/pension/pension.php">pension</a>
-            <a href="../../pages/photo/photo.php">photo</a>
-            <a href="../../pages/prend/prend.php">prend</a>
-            <a href="../../pages/race/race.php">race</a>
-            <a href="../../pages/robe/robe.php">robe</a>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+</head>
+<body>
+    <header class="header-container">
+        <div class="user-info">
+            <button id="menu-button">Menu</button>
+            <span>Bienvenue, <strong><?php echo htmlspecialchars($user_pseudo); ?></strong></span>
+            <form action="" method="post" class="logout-form" style="all:unset">
+                <button type="submit" name="logout">Déconnexion</button>
+            </form>
         </div>
+    </header>
+
+    <div class="sidebar" style="z-index: 0; height: 100%; overflow-y: auto;">
+        <h2 style="text-align: center; color: white; margin: 20px 0;">Menu</h2>
+        <a href="../../pages/calendrier/calendrier.php">Calendrier</a>
+        <a href="../../pages/cavalerie/cavalerie.php">Cavalerie</a>
+        <a href="../../pages/cavalier/cavalier.php">Cavalier</a>
+        <a href="../../pages/commune/commune.php">Commune</a>
+        <a href="../../pages/cours/cours.php">Cours</a>
+        <a href="../../pages/evenement/evenement.php">Événement</a>
+        <a href="../../pages/galop/galop.php">Galop</a>
+        <a href="../../pages/participation/participation.php">Participation</a>
+        <a href="../../pages/pension/pension.php">Pension</a>
+        <a href="../../pages/photo/photo.php">Photo</a>
+        <a href="../../pages/prend/prend.php">Prend</a>
+        <a href="../../pages/race/race.php">Race</a>
+        <a href="../../pages/robe/robe.php">Robe</a>
     </div>
-</header>
-<?php
 
+    <div class="content">
+        <?php
+        include '../../pages/cavalerie/cavalerie.class.php';
+        include '../../pages/cavalier/cavalier.class.php';
+        include '../../pages/commune/commune.class.php';
+        include '../../pages/cours/cour.class.php';
+        include '../../pages/galop/galop.class.php';
+        include '../../pages/participation/participation.class.php';
+        include '../../pages/pension/pension.class.php';
+        include '../../pages/photo/photo.class.php';
+        include '../../pages/race/race.class.php';
+        include '../../pages/robe/robe.class.php';
+        ?>
+    </div>
 
-
-
-
-
-//include '../../pages/calendrier/calendrier.class.php';
-include '../../pages/cavalerie/cavalerie.class.php';
-include '../../pages/cavalier/cavalier.class.php';
-include '../../pages/commune/commune.class.php';
-include '../../pages/cours/cours.class.php';
-//include '../../pages/evenement/evenement.class.php';
-include '../../pages/galop/galop.class.php';
-include '../../pages/participation/participation.class.php';
-include '../../pages/pension/pension.class.php';
-//include '../../pages/photo/photo.class.php';
-include '../../pages/prend/prend.class.php';
-include '../../pages/race/race.class.php';
-include '../../pages/robe/robe_class.php';
-
-?>
-
-<!-- FULL Calendar -->
-
-
+    <!-- Flèche pour remonter en haut -->
+    <div id="scroll-to-top" class="scroll-to-top" style="display: none;">
+        &#8679; <!-- Flèche vers le haut -->
+    </div>
+</body>
+</html>
